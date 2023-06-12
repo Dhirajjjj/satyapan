1af448408e59bc324ed6df3ad5ec026d730bef74
Dhirajjjj <69754979+Dhirajjjj@users.noreply.github.com>
2023-06-12 17:21:15 +0530

    Update App front

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
+        <h1>Satyapan</h1>
       </header>
     </div>
   );
