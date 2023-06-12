e2423157a439275221e3af0b59b78a1ff599b065
Dhirajjjj <69754979+Dhirajjjj@users.noreply.github.com>
2023-06-12 17:38:21 +0530

    Update App content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Satyapan</h1>
+        <p>A simpler way for identity verification</p>
       </header>
     </div>
   );
